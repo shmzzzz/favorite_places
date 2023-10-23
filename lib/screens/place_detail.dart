2610aeaac180c5b820be4d405221d680b3303f5d
shmzzzz <slt0623@gmail.com>
2023-10-23 20:46:23 +0900

    ✨ 詳細画面でmapが表示されるようにする

--- lib/screens/place_detail.dart
+++ lib/screens/place_detail.dart
@@ -10,6 +10,12 @@ class PlaceDetailScreen extends StatelessWidget {
 
   final Place place;
 
+  String get locationImage {
+    final lat = place.location.latitude;
+    final lng = place.location.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyC7CIdjczr_x5rBiJbXECbOeIcjcb6MeGc';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +31,45 @@ class PlaceDetailScreen extends StatelessWidget {
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(
+                    locationImage,
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 16,
+                  ),
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.transparent,
+                        Colors.black54,
+                      ],
+                      begin: Alignment.center,
+                      end: AlignmentDirectional.bottomCenter,
+                    ),
+                  ),
+                  child: Text(
+                    place.location.address,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
